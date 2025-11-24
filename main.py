266ed11a959fb63167678fdb5945ef3c442907a0
prakashsingh62 <prakashkumarsing@gmail.com>
2025-11-24 22:00:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from flask import Flask, render_template, request, redirect, jsonify
-import json
-import logging
-
+from flask import Flask
 from dashboard.backend_api import api_blueprint
 
 app = Flask(__name__)
 
-# Register API blueprint  
+# Register API blueprint
 app.register_blueprint(api_blueprint, url_prefix="/api")
-
 
 @app.route("/")
 def home():
     return "RFQ Level 6 Backend is Running"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
